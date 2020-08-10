9ad9f358f50310d348623f8f77e55ba37d3338d9
Musa817 <55715717+Musa817@users.noreply.github.com>
2020-08-10 09:44:51 -0400

    Add files via upload

---
+++ Final_Project/js/main.js
@@ -0,0 +1,45 @@
+//Creating a button and running a function if said button is clicked.
+let searchButton = document.getElementById("search")
+searchButton.addEventListener("click", onClick);
+function onClick(event){
+event.preventDefault();
+let inputElement = document.getElementById("keyword");
+let userInput = inputElement.value;
+//Fetching some bill data from the ProPublica Congress API.
+fetch(' https://api.propublica.org/congress/v1/bills/search.json?query=' + userInput,{//opening fetch function.
+	headers: new Headers({//opening headers.
+		"X-API-Key": "rGAZ6C77Cn1BKLZ0qmYlQNpylB6e2EJG5f50g4pS"
+	})//closing headers.
+})//closing fetch function.
+	.then(function(res) {
+		return res.json();
+	
+	})
+	//Displaying bill information and links onto the page.
+	.then(function(data) {
+		let billSpace = document.getElementById("Bills");
+	 const billArray  = data.results[0].bills;
+		for(let i = 0; i < billArray.length; i++){
+		let billTitle = document.createElement("p");
+		let billSummary = document. createElement("p");
+		let billUrl = document.createElement("a");
+		let URL = billArray[i].congressdotgov_url;
+		billUrl.textContent = "More about the bill.";
+		billUrl.href = URL;
+		billTitle.innerText = "Title:" + " " + billArray[i].title;
+		billSummary.innerText = "Summary:" + " " + billArray[i].summary_short;
+		billUrl.href = billArray[i].congressdotgov_url;
+        billSpace.appendChild(billTitle);
+     	billSpace.appendChild(billSummary);
+		billSpace.appendChild(billUrl);
+		}
+		console.log(billArray[7]);
+		inputElement.value = "";
+	
+	})
+	.catch(function(error) {
+		console.log("ERROR:", error);
+		inputElement.value = "";
+	
+	});
+}
